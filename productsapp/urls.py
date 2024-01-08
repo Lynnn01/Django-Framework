087766b303940fde56cb66168a2d26257764f889
Lynnn01 <jetsadathegame@gmail.com>
2024-01-08 14:17:06 +0700

    template and routing

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -2,5 +2,7 @@ from productsapp import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("product/details/<int:id>", views.productDetail, name="productDetail"),
+    path("products", views.products, name="product"),
 ]
--- productsapp/views.py
+++ productsapp/views.py
@@ -6,7 +6,14 @@ from productsapp.models import *
 def index(request):
     products = Product.objects.filter(isTrending=True)
     print(products)
-    return render(request,
-                  'index.html',
-                  {"products": products},
-                  )
+    return render(request, 'index.html', {"products": products},)
+
+
+def productDetail(request, id):
+    product_detail = Product.objects.get(id=id)
+    return render(request, 'detail.html', {"details": product_detail},)
+
+
+def products(request, ):
+    products = Product.objects.all()
+    return render(request, 'product.html',{"products": products})
